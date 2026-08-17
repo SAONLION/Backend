a6560fa1cd0cff32c982fd6e73c630f6b1bdb5a7
jin <connected0180@gmail.com>
2026-08-17 16:00:26 +0900

    fix: CORS 설정누락 수정

--- src/main/java/mcm/mcmAI/global/WebConfig.java
+++ src/main/java/mcm/mcmAI/global/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "https://*.vercel.app",       // Vercel 프리뷰/변경 대응
                         "https://www.tagonai.site",   // 확정된 정식 도메인
                         "https://tagonai.site",        // www 없는 버전도 혹시 몰라
+                        "https://api.tagonai.site",    // Swagger UI가 자기 자신(API 서버)을 호출할 때의 origin
                         "http://localhost:5173"        // 로컬 프론트 개발 서버
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
